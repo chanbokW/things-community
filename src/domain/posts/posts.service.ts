95b0aa20d7b9f3bc965e862030a09bd14b23eb0c
chanbokW <cksqhr4961@naver.com>
2022-09-06 20:24:56 +0900

    Feat: [#1] 게시글 생성

--- src/domain/posts/posts.service.ts
+++ src/domain/posts/posts.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePostsDto } from './dto/create-posts.dto';
+import { Posts } from './entity/posts.entity';
+import * as bcrypt from 'bcrypt';
+import { PostsResponse } from './dto/posts-response.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Posts) private postsRepository: Repository<Posts>,
+  ) {}
+  /**
+   *
+   * @param createPostsDto 게시글 생성
+   * @returns 게시글 생성 번호
+   */
+  async create(createPostsDto: CreatePostsDto): Promise<number> {
+    const password = createPostsDto.Password;
+    // 비밀번호 암호화
+    const hashpassword = await bcrypt.hash(password, 10);
+
+    const saveposts = await this.postsRepository.save(
+      createPostsDto.toEntity(hashpassword),
+    );
+
+    return saveposts.id;
+  }
+}
