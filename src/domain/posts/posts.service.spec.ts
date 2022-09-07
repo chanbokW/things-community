3f219c0bddd4e5f99c88e2470082d998030cb9ac
chanbokW <cksqhr4961@naver.com>
2022-09-07 14:39:39 +0900

    Fix: validation 값 수정

--- src/domain/posts/posts.service.spec.ts
+++ src/domain/posts/posts.service.spec.ts
@@ -1,15 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmAsyncModuleOptions } from 'src/global/config/typeorm.config';
-import { Repository } from 'typeorm';
-import { CreatePostsDto } from './dto/create-posts.dto';
-import { Posts } from './entity/posts.entity';
-import { PostsModule } from './posts.module';
 import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
-  let postsRepository: Repository<Posts>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
